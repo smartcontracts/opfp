ea2bd347e3c78f755ef4161c6ab0792be1093177
Imran Jami <imranajami@gmail.com>
2022-08-24 01:47:16 -0700

    added button + trait cards

---
+++ packages/frontend/src/components/TraitCard/index.tsx
@@ -0,0 +1,1 @@
+export { TraitCard } from './TraitCard'
--- packages/frontend/src/pages/home/Home.tsx
+++ packages/frontend/src/pages/home/Home.tsx
@@ -1,11 +1,60 @@
+import { Button } from '../../components/Button'
 import { MirrorCard } from '../../components/MirrorCard'
+import { TraitCard } from '../../components/TraitCard'
 import { AppLayout } from '../../layout/AppLayout/AppLayout'
 import './Home.scss'
 
 export const Home = () => {
+  const traits = [
+    {
+      name: 'background',
+      value: 'yellow',
+    },
+    {
+      name: 'helmet',
+      value: 'flower',
+    },
+    {
+      name: 'visor',
+      value: 'sky',
+    },
+    {
+      name: 'chain',
+      value: 'silver',
+    },
+    {
+      name: 'body',
+      value: 'robotaskj aksks hvh ',
+    },
+    {
+      name: 'outfit',
+      value: 'camo tee blah blah',
+    },
+  ]
+
+  const updateNFT = () => {
+    console.log('updateNFT')
+  }
+
   return (
-    <div className="Home">
-      <AppLayout mirrorCard={<MirrorCard />} content={<h1>Content</h1>} />
+    <div className="home__container">
+      <AppLayout
+        mirrorCard={<MirrorCard />}
+        content={
+          <div className="home__content">
+            <h3>Optimism</h3>
+            <h1>0x123...123</h1>
+            <div className="card__container">
+              {traits.map((trait) => (
+                <TraitCard name={trait.name} value={trait.value} />
+              ))}
+            </div>
+            <Button onClick={updateNFT}>
+              <span>Update NFT</span>
+            </Button>
+          </div>
+        }
+      />
     </div>
   )
 }
